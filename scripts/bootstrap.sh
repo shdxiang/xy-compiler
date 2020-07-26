5e001a0dc4ed218af7cef46534fdd0c92474037b
LonghronShen <LonghronShen@hotmail.com>
2020-07-26 19:05:14 +0800

    fix build for macOS

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y git build-essential flex bison llvm-3.8* libedit-dev
+sudo apt-get install -y git build-essential flex bison llvm-10* libedit-dev
