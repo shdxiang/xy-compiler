35206529edbce14ae37e60258e7f55c253a4fb4b
Steven Hangger <longhronshen@hotmail.com>
2020-07-27 09:10:29 +0800

    update

--- src/compiler/main.cpp
+++ src/compiler/main.cpp
@@ -1,16 +1,38 @@
-#include "ast.h"
-#include "gen.h"
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 
+
+#include <cxxopts.hpp>
+
+#include "ast.h"
+#include "gen.h"
+
 using namespace std;
+
+extern FILE *yyin = NULL, *yyout = NULL;
 
 extern int yyparse();
 
 extern NBlock *programBlock;
 
 int main(int argc, char **argv) {
+  cxxopts::Options options("The xy Language Compiler",
+                           "A toy complier based on LLVM JIT.");
+  options.add_options()
+      ("d,debug", "Enable debugging")
+      ("f,file", "File name", cxxopts::value<std::string>())
+      ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"));
+
+  auto result = options.parse(argc, argv);
+  if (result.count("f,file") > 0)
+  {
+     auto inputFilePath = result["f,file"].as<std::string>();
+    
+  }
+
   yyparse();
-  cout << programBlock << endl;
+  std::cout << programBlock << endl;
   InitializeNativeTarget();
   InitializeNativeTargetAsmPrinter();
   InitializeNativeTargetAsmParser();
